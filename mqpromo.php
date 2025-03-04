5576dd0e07655c404073f1f682d095cc2d5f1bcd
thybux <faivre.thomas88@free.fr>
2025-03-04 17:21:14 +0100

    update

--- mqpromo.php
+++ mqpromo.php
@@ -1,4 +1,7 @@
 <?php
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
 
 class MqPromo extends Module
 {
@@ -14,9 +17,7 @@ class MqPromo extends Module
             'max' => _PS_VERSION_
         ];
         $this->bootstrap = true;
-
         parent::__construct();
-
         $this->displayName = $this->l('MQ Promotions');
         $this->description = $this->l('Display promotions on your homepage');
         $this->confirmUninstall = $this->l('Are you sure you want to uninstall?');
@@ -34,40 +35,80 @@ class MqPromo extends Module
         return parent::uninstall();
     }
 
-    
-   
     public function hookDisplayHome($params)
     {
-      $promo = "salut";
-      $this->context->smarty->assign([
-        "promo" => $promo
-      ]);
-      return $this->fetch('module:' . $this->name . '/views/template/mqpromo.tpl');
+        $promo = $this->getHomePromo();
+
+        $enrichedProducts = [];
+
+        foreach ($promo as $product) {
+            $id_product = (int)$product['id_product'];
+            $productObj = new Product($id_product, true, $this->context->language->id);
+
+            if (!Validate::isLoadedObject($productObj)) {
+                continue;
+            }
+
+            $images = Image::getImages($this->context->language->id, $id_product);
+            $id_image = !empty($images) ? $images[0]['id_image'] : null;
+
+            $specific_prices = SpecificPrice::getByProductId($id_product);
+
+            $productInfo = [
+                'id_product' => $id_product,
+                'reference' => $product['reference'],
+                'name' => $product['name'],
+                'price' => $product['price'],
+                'price_without_reduction' => $productObj->getPriceWithoutReduct(),
+                'specific_prices' => !empty($specific_prices),
+                'id_image' => $id_image,
+                'link_rewrite' => $productObj->link_rewrite
+            ];
+
+            $enrichedProducts[] = $productInfo;
+        }
+
+        dump($enrichedProducts);
+
+        $this->context->smarty->assign([
+            "promo" => $enrichedProducts
+        ]);
+
+        return $this->fetch('module:' . $this->name . '/views/templates/mqpromo.tpl');
+
     }
 
-   public function hookDisplayHeader()
+    public function hookDisplayHeader()
     {
-
         $this->context->controller->registerStylesheet(
-            'mqreviews-style-reviews',
-            'modules/'.$this->name.'/views/css/reviews.css',
+            'mqpromo-style',
+            'modules/'.$this->name.'/views/css/mqpromo.css',
             ['media' => 'all', 'priority' => 200]
         );
 
-        $this->context->controller->registerStylesheet(
-            'mqreviews-style-reassurance',
-            'modules/'.$this->name.'/views/css/reassurance.css',
-            ['media' => 'all', 'priority' => 200]
+        $this->context->controller->registerJavascript(
+            'mqpromo-js',
+            'modules/'.$this->name.'/views/js/mqpromo.js',
+            ['position' => 'bottom', 'priority' => 200]
         );
 
-        $this->context->controller->registerStylesheet(
-            'material-icons',
-            'https://fonts.googleapis.com/icon?family=Material+Icons',
-            ['server' => 'remote', 'media' => 'all', 'priority' => 200]
-        );
      }
 
-  }
+    public function getHomePromo(){
+        $result = $this->getPromoFromBdd();
+        return $result;
+    }
 
-
-?>
+    public function getPromoFromBdd(){
+        $db = Db::getInstance();
+        $id_lang = (int)Context::getContext()->language->id;
+        $sql = 'SELECT p.reference, p.id_product, p.price, pl.name
+            FROM ' . _DB_PREFIX_ . 'product p
+            LEFT JOIN ' . _DB_PREFIX_ . 'product_lang pl ON (p.id_product = pl.id_product AND pl.id_lang = ' . $id_lang . ')
+            WHERE p.on_sale = 1
+            AND p.active = 1
+            LIMIT 5';
+        $result = $db->executeS($sql);
+        return $result;
+    }
+}
